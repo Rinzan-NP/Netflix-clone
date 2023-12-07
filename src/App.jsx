5e474e46fb63a68f09da48efbdfd0dce0944cfb4
Rinzan-NP <nprinzan@gmail.com>
2023-12-07 13:46:51 +0530

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Banner from './components/Banner/Banner'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Banner/>
+      
     </>
   )
 }
